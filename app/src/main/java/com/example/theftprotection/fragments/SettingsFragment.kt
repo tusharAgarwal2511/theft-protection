cfa51991b324ce7e39d2a2164d07422a3bcdb76c
Tushar <agarwal25112000@gmail.com>
2023-03-02 15:27:46 +0530

    added light and light mode

--- app/src/main/java/com/example/theftprotection/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/theftprotection/fragments/SettingsFragment.kt
@@ -1,64 +1,70 @@
 package com.example.theftprotection.fragments
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import com.example.theftprotection.databinding.FragmentSettingsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class SettingsFragment : Fragment() {
 
     lateinit var settingsFragmentBinding : FragmentSettingsBinding
     // TODO: Rename and change types of parameters
-    private var param1 : String? = null
-    private var param2 : String? = null
-
-    override fun onCreate(savedInstanceState : Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater : LayoutInflater, container : ViewGroup?,
         savedInstanceState : Bundle?
     ) : View? {
 
+
+
+
         settingsFragmentBinding = FragmentSettingsBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
+
+
+        var isThemeDark: Boolean = false
+
+        when (context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> {
+                isThemeDark = true
+//                settingsFragmentViewModel.changeTheme(isThemeDark)
+                settingsFragmentBinding.settingsFragmentThemeChangeSwitch.isChecked = true
+            }
+            Configuration.UI_MODE_NIGHT_NO -> {
+                isThemeDark = false
+//                settingsFragmentViewModel.changeTheme(isThemeDark)
+                settingsFragmentBinding.settingsFragmentThemeChangeSwitch.isChecked = false
+            }
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                isThemeDark = false
+//                settingsFragmentViewModel.changeTheme(isThemeDark)
+                settingsFragmentBinding.settingsFragmentThemeChangeSwitch.isChecked = false
+            }
+        }
+
+
+
+
+        settingsFragmentBinding.settingsFragmentThemeChangeSwitch?.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+            } else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
         return settingsFragmentBinding.root
+
+//        settingsFragmentBinding.settingsFragmentThemeChangeSwitch.isSelected = false
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1 : String, param2 : String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
